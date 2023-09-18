79004560a784d9e968d72cfd0a58a9515522e052
MrV1ctor <vurumov25@pascack.org>
2023-09-18 11:18:06 -0400

    ADDED ACCORDION ON SCAN CLICK
    AHHAHAHHAHAHHAHAHA
    todo: populate studentsData with actual data, change createAccordion function to match the new data

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let studentData = [];
+
 const custom = document.querySelector("#custom");
 const opts = ["Counselors", "Administrators", "Students", "Everyone"]
 
@@ -43,3 +46,78 @@ const mainInterval = setInterval(() => {
 document.addEventListener("click", () => {
     interacted = true;
 })
+
+
+function scan() {
+
+
+    console.log("SCAN IN MY ASS AHHAHAHAHAHHAHA");
+
+    populateStudentData();
+    createAccordion(studentData);
+
+}
+
+document.getElementById("scan-button").addEventListener("click", scan);
+
+function createAccordion(studentData) {
+
+    //the html layout below for each studen in studentData list
+
+    //TODO: make actual html for each student
+    studentData.forEach(student => {
+        const studentHTML = `
+        <button class="accordion">${student.name}</button>
+        <div class="panel">
+            <p>${student.bio}</p>
+        </div>
+        `
+        document.getElementById("accordion-div").innerHTML += studentHTML;
+    });
+
+    //accordion functionality
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+    }
+
+}
+
+function populateStudentData() {
+
+    studentData = [
+        {
+            name : "John Doe",
+            bio : "John is a senior at Westview High School. He is interested in computer science and math. He is also a member of the Westview Robotics team."
+        },
+        {
+            name : "Jane Doe",
+            bio : "Jane is a senior at Westview High School. She is interested in computer science and math. She is also a member of the Westview Robotics team."
+        },
+        {
+            name : "Johnny Doe",
+            bio : "Johnny is a senior at Westview High School. He is interested in computer science and math. He is also a member of the Westview Robotics team."
+        }
+    ]
+    
+}
+
+/*
+
+        <button class="accordion">Section 3</button>
+        <div class="panel">
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+
+*/
+
